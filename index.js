a43e8659434ebb633f93a0105442205446293f5d
MrHatkeeper <karel.krch@educnaet.cz>
2025-03-06 16:53:52 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+let tasksFolder = "/tasks"
 
+function createTask() {
+    let name = document.getElementById("name").value;
+    let date = document.getElementById("date").value;
+    let text = document.getElementById("text").value;
 
+    let output = '{' +
+        '"Name":"' + name + '",' +
+        '"Date":"' + date + '",' +
+        '"Text":"' + text + '' +
+        '"}';
 
-
-function createTask(){
-console.log("task created...");
-
+    console.log(output);
+    let a = JSON.parse(output)
+    console.log(a);
 }
